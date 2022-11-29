e9daa5b1c1b04eca781c82d559922b891f5e7b3a
Haldun Halil Olcay <halil.olcay@setur.local>
2022-11-29 22:36:47 +0300

    set the data to textView and display on the emulator

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-
+import kotlinx.android.synthetic.main.activity_main.*
 const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,11 +18,29 @@ class MainActivity : AppCompatActivity() {
 
         getMyData();
     }
-    private fun getMyData(){
-        val retrofitBuilder = Retrofit.Builder()
-            .baseUrl("https://jsonplaceholder.typicode.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(ApiInterface::class.java)
+
+    private fun getMyData() {
+        val retrofitBuilder =
+            Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
+                .build().create(ApiInterface::class.java)
+
+        val retrofitData = retrofitBuilder.getData()
+
+        retrofitData.enqueue(object : Callback<List<MyDataItem>?> {
+            override fun onResponse(call: Call<List<MyDataItem>?>, response: Response<List<MyDataItem>?>) {
+               val responseBody = response.body()!!
+
+                val myStringBuilder = StringBuilder()
+                for (data in responseBody) {
+                    myStringBuilder.append(data.id)
+                    myStringBuilder.append("\n")
+                }
+               txtId.text = myStringBuilder
+            }
+
+            override fun onFailure(call: Call<List<MyDataItem>?>, t: Throwable) {
+                Log.d("MainActivity", "onFailure: ${t.message}")
+            }
+        })
     }
 }
